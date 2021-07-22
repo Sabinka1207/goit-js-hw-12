05a62d850d93e95366a6d504f3e221f0c7db49ba
Sabina Abdulaliieva <77077884+Sabinka1207@users.noreply.github.com>
2021-07-22 19:29:47 +0300

    check

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import './css/styles.css';
-// import debounce from "lodash.debounce";
 var debounce = require('lodash.debounce');
+
 const DEBOUNCE_DELAY = 300;
 
-console.log(debounce)
-
 const input = document.querySelector("#search-box")
-input.addEventListener('input', onInput)
+input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+
 
 function onInput(evt) {
-    debounce(debounced, 3000)
-
-}
-
-function debounced() {
     console.log(evt.target.value)
-    console.log("OK")           
 }
+
